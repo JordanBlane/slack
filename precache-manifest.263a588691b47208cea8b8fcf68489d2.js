bebd350ea6aaca9781c11381057df17d0144b18e
JordanBlane <jordan26265071@live.com>
2019-11-22 17:26:51 +0000

    Updates

--- precache-manifest.263a588691b47208cea8b8fcf68489d2.js
+++ precache-manifest.263a588691b47208cea8b8fcf68489d2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1e7d30983cc0294c326dbc18ae3847d6",
+    "revision": "9160dc4fe69f5abb6e02b1f211247ae8",
     "url": "/slack/index.html"
   },
   {
-    "revision": "48bdc02c054f41d3a75c",
+    "revision": "d9052453667600b7c06c",
     "url": "/slack/static/css/main.f8e9fa17.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/slack/static/js/2.fa2449c8.chunk.js"
   },
   {
-    "revision": "48bdc02c054f41d3a75c",
-    "url": "/slack/static/js/main.65840dfd.chunk.js"
+    "revision": "d9052453667600b7c06c",
+    "url": "/slack/static/js/main.2416101e.chunk.js"
   },
   {
     "revision": "e3a2cb54ce8b2f6e84cf",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/slack/precache-manifest.50343c6f187af97e2f3f840075e291a0.js"
+  "/slack/precache-manifest.263a588691b47208cea8b8fcf68489d2.js"
 );
 
 self.addEventListener('message', (event) => {
